bf749481ffe53189bc2aef0dfbe146019b0cdecc
Tohir <tohirdavrsmart@gmail.com>
2024-07-23 16:45:37 +0500

    fix some bugs end add PORT

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ import commands from './commands/index.js';
 import controllers from './controllers/controller.js';
 import middleware from './middleware/index.js';
 import db from "./config/db.js"
+import express from "express";
+
+const app = express()
+
+const PORT = process.env.PORT || 8888
 
 const bot = new Telegraf(token);
-db()
 middleware(bot);
 commands(bot);
 controllers(bot);
 bot.launch();
+
+
+//start server
+!async function () {
+    try {
+        db()
+    } catch (error) {
+        console.log(error);
+    }
+    app.listen(PORT, () => console.log(`🚀 BackEnd server is running http://localhost:` + PORT))
+}();
